fb978746eca0dfb85e84b931fc85e0f0a75b304f
stp.do <sittipol1123@gmail.com>
2023-02-22 23:06:16 +0700

    first-day

---
+++ src/pages/test.tsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import Paperbase from '@/component/layout'
+
+export default function test() {
+  return (
+    <Paperbase>
+        <div>
+            test
+        </div>
+    </Paperbase>
+  )
+}
